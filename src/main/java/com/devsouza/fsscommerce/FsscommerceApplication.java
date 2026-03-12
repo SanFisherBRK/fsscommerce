b18475943f7adf54852326f3f32bcca7d6830f0c
Souza <sanfisher@msn.com>
2026-03-12 14:23:54 -0300

    Criação do projeto

---
+++ src/main/java/com/devsouza/fsscommerce/FsscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devsouza.fsscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsscommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsouza/fsscommerce/FsscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsouza.fsscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
